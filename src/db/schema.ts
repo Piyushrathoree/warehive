d15ff05e9fa3001b0f36c0283e0c006db13d72b8
Piyushrathoree <piyushrathore.works@gmail.com>
2026-09-22 00:12:42 +0530

    02_configured the schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,80 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  varchar,
+  uuid,
+  timestamp,
+  integer,
+  text,
+  pgEnum,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  create_at: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
 });
 
+export const tenants = pgTable("tenants", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: varchar({ length: 255 }).notNull().unique(),
+  hash: uuid("api_key").references(() => apiKeys.id, {
+    onDelete: "cascade",
+  }),
+});
 
+export const apiKeys = pgTable("api_keys", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  hash: varchar({ length: 512 }).unique(),
+  createAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  expiresAt: timestamp("expires_at", { withTimezone: true }),
+});
+
+export const tasks = pgTable("tasks", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("userId").references(() => users.id, {
+    onDelete: "cascade",
+  }),
+  title: varchar({ length: 255 }).notNull(),
+  description: text("description"),
+  createAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  tenantId: uuid("tenantId").references(() => tenants.id, {
+    onDelete: "cascade",
+  }),
+});
+
+export const membershipRoleEnum = pgEnum("membership_role", [
+  "owner",
+  "admin",
+  "member",
+]);
+export const membership = pgTable("membership", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("userId").references(() => users.id, { onDelete: "cascade" }),
+  tenantId: uuid("tenantId").references(() => tenants.id, {
+    onDelete: "cascade",
+  }),
+  role: membershipRoleEnum("role").notNull().default("member"),
+  createAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
